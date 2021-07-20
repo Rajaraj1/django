d242401b1d76c82a80c530ca5b7c9bf754b621d3
Raja raj <rr702678@gmail.com>
2021-07-20 22:13:42 +0530

    This is my First commit

---
+++ icoder/views.py
@@ -0,0 +1,13 @@
+from django.http import request
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+def form(request):
+    a = request.GET.get('fname')
+    b = request.GET.get('fpass')
+    c = request.GET.get('ftext')
+    d = request.GET.get('fcheck')
+    dic = {'name': a,'pass':b,'text':c,'check':d}
+    return render(request,'form.html',dic)
